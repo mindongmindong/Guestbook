2a0756447ae10d0b9a020ce6b746b97077f1d575
mindongmindong <dcmin123@ajou.ac.kr>
2024-02-11 17:02:18 +0900

    [ADD]File structure

---
+++ src/main/java/com/mindong/guestbook/entity/Guestbook.java
@@ -0,0 +1,14 @@
+package com.mindong.guestbook.entity;
+
+import jakarta.persistence.Entity;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Guestbook extends BaseEntity {
+
+}
